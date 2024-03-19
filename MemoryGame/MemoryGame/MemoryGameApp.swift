f743439d60a9491d2caf0e629bf116797c29146c
Jose Folgar <f.jose@wustl.edu>
2024-03-18 20:07:38 -0500

    Initial Project Creation

---
+++ MemoryGame/MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Jose Folgar on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
